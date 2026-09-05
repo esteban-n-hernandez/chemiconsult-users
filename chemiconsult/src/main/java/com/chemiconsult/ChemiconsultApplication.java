ac1fbc8d3caf01d3bacf203ace8fc315c2bd9bd2
esteban <esteban_hernandez@outlook.com>
2026-09-05 08:56:47 -0300

    cambio banner app

--- chemiconsult/src/main/java/com/chemiconsult/ChemiconsultApplication.java
+++ chemiconsult/src/main/java/com/chemiconsult/ChemiconsultApplication.java
@@ -3,8 +3,6 @@ package com.chemiconsult;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 import java.util.TimeZone;
 
 @SpringBootApplication
@@ -14,10 +12,8 @@ public class ChemiconsultApplication implements CommandLineRunner {
         SpringApplication.run(ChemiconsultApplication.class, args);
     }
 
-
     @Override
     public void run(String... args) {
         TimeZone.setDefault(TimeZone.getTimeZone("America/Argentina/Buenos_Aires"));
-        System.out.println("App Chemiconsult inicializada a las: " + java.time.LocalDateTime.now() + " en la zona horaria: " + TimeZone.getDefault().getID());
     }
 }
